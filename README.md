0e52fc296a7121de6f54c4023c03778e0773369f
Tusharsharma9759 <114795523+Tusharsharma9759@users.noreply.github.com>
2024-11-21 14:25:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Ecommerce-website
+This is a shopping website of electronic items like laptops,mobile,phones,earphones. You can add items to the cart and find the total items added and the price in the cart page.
+You can also remove items as your wish. I have created this website using json. Products are added in this website using json.
---
+++ data.js
@@ -0,0 +1,234 @@
+let shopItemsData = [
+    {
+    "id": 1,
+    "name": "Dell Inspiration Notebook",
+    "price": 79905.0,
+    "desc": "Croma",
+    "img": "/images/product1.png",
+    
+},
+{
+    "id": 2,
+    "name": "Hp Standard Laptop ",
+    "price": 37500,
+    "desc": "Reliance Digital",
+    "img": "/images/product2.png",
+   
+}, 
+{
+    "id": 3,
+    "name": "ASUS TUF Gaming F15",
+    "price": 69990,
+    "desc": "Croma",
+    "img": "/images/product3.png"
+   
+}, 
+{
+    "id": 4,
+    "name": "Samsung Galaxy Book3 (13th Gen)",
+    "price": 89990,
+    "desc": "Reliance Digital",
+    "img": "/images/product4.png"
+    
+}, 
+{
+    "id": 5,
+    "name": "Hp Victus 15 Gaming",
+    "price": 69499,
+    "desc": "Reliance Digital",
+    "img": "/images/product5.png"
+    
+}, 
+{
+    "id": 6,
+    "name": "Lenovo IdeaPad 3 Intel",
+    "price": 26900,
+    "desc": "Windows OS",
+    "img": "/images/product6.png"
+    
+}, 
+{
+    "id": 7,
+    "name": "ASUS MB711WS Vivobook 16X",
+    "price": 59499,
+    "desc": "Reliance Digital",
+    "img": "/images/product7.png"
+  
+}, 
+{
+    "id": 8,
+    "name": "Zebronics Pro Y Intel Core I5",
+    "price": 34699,
+    "desc": "Zebronics",
+    "img": "/images/product8.png"
+   
+},
+{
+    "id": 9,
+    "name": "Lenovo IdeaPad Intel Core i5",
+    "price": 45999,
+    "desc": "Lenovo",
+    "img": "/images/product9.png"
+},
+{
+    "id": 10,
+    "name": "Acer Aspire 5 Intel Core i5",
+    "price": 56499,
+    "desc": "Croma",
+    "img": "/images/product10.png"
+},
+{
+    "id": 11,
+    "name": "Asus Vivobook Gaming i5",
+    "price": 78900,
+    "desc": "Asus",
+    "img": "/images/product11.png"
+},
+{
+    "id": 12,
+    "name": "Lenovo ThinkPad i7 14th Gen",
+    "price": 89000,
+    "desc": "Amazon",
+    "img": "/images/product12.png"
+},
+{
+    "id": 13,
+    "name": "Acer Nitro Gaming i5",
+    "price": 67890,
+    "desc": "Acer",
+    "img": "/images/product13.png"
+},
+{
+    "id": 14,
+    "name": "Asus Vivobook Intel Core i5",
+    "price": 45799,
+    "desc": "Amazon",
+    "img": "/images/product14.png"
+},
+{
+    "id": 15,
+    "name": "Acer Aspire 5 Gaming 13th Gen ",
+    "price": 62999,
+    "desc": "Acer",
+    "img": "/images/product15.png"
+},
+{
+    "id": 16,
+    "name": "Lenovo IdeaPad Intel Core i7",
+    "price": 78599,
+    "desc": "Lenovo",
+    "img": "/images/product16.png"
+},
+{
+    "id": 17,
+    "name": "JBL HeadPhones With Noise cancellation",
+    "price": 5999,
+    "desc": "JBL",
+    "img": "/images/product17.png"
+},
+{
+    "id": 18,
+    "name": "Samsung Galaxy A15 128GB RAM-8G",
+    "price": 19999,
+    "desc": "Samsung",
+    "img": "/images/product18.png"
+},
+{
+    "id": 19,
+    "name": "Wireless Neckband noise cancellation",
+    "price": 78999,
+    "desc": "Neckband",
+    "img": "/images/product19.png"
+},
+{
+    "id": 20,
+    "name": "OPPO A38 RAM-8GB 128GB Memory",
+    "price": 78599,
+    "desc": "Lenovo",
+    "img": "/images/product20.png"
+},
+{
+    "id": 21,
+    "name": "JBL Earphones with noise cancellatiion",
+    "price": 899,
+    "desc": "JBL",
+    "img": "/images/product21.png"
+},
+{
+    "id": 22,
+    "name": "Wireless NeckBand good quality sound",
+    "price": 1299,
+    "desc": "NeckBand",
+    "img": "/images/product22.png"
+},
+{
+    "id": 23,
+    "name": "Vivo Y200 5G 8G-RAM | 128GB Memory",
+    "price": 7599,
+    "desc": "Lenovo",
+    "img": "/images/product23.png"
+},
+{
+    "id": 24,
+    "name": "BDSE Headsets WireLess Noise Cancellation ",
+    "price": 7999,
+    "desc": "BDSE",
+    "img": "/images/product24.png"
+},
+{
+    "id": 25,
+    "name": "OPPO A59 5G 8G-RAM | 128GB Memory",
+    "price": 29789,
+    "desc": "OPPO",
+    "img": "/images/product25.png"
+},
+{
+    "id": 26,
+    "name": "Redmi Note 11 5G 8G-RAM | 128GB Memory",
+    "price": 17599,
+    "desc": "Redmi",
+    "img": "/images/product26.png"
+},
+{
+    "id": 27,
+    "name": "Redmi Note 12 5G 8G-RAM | 128GB Memory",
+    "price": 20599,
+    "desc": "Lenovo",
+    "img": "/images/product27.png"
+},
+{
+    "id": 28,
+    "name": "Hammer Headsets Noise Cancellation",
+    "price": 7599,
+    "desc": "Hammer",
+    "img": "/images/product28.png"
+},
+{
+    "id": 29,
+    "name": "Galaxy Tab A9 | 4GB | 64GB |",
+    "price": 12599,
+    "desc": "Samsung",
+    "img": "/images/product29.png"
+},
+{
+    "id": 30,
+    "name": "Airdopes 8 WireLess Earbuds",
+    "price": 599,
+    "desc": "Boat",
+    "img": "/images/product30.png"
+},
+{
+    "id": 31,
+    "name": "Lenovo Tab M7 3rd Gen",
+    "price": 7700,
+    "desc": "Lenovo",
+    "img": "/images/product31.png"
+},
+{
+    "id": 32,
+    "name": "JBL Tune 130NC BT v5.2",
+    "price": 4999,
+    "desc": "JBL",
+    "img": "/images/product32.png"
+}
+];
